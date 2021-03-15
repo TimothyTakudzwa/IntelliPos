0df693252b1adec44076d1e4d042b6a4a251e799
Timoth Ngorima <timothytakudzwa@gmail.com>
2021-03-15 15:18:19 +0200

    Number of Tries

--- merchant/models.py
+++ merchant/models.py
@@ -67,6 +67,8 @@ class User(AbstractUser):
     phone_number = models.CharField(max_length=20, default='263')
     role = models.CharField(max_length=20, default='',
                             choices=(('ADMIN', 'ADMIN'), ('TELLER', 'TELLER')))
+    pin_tries = models.IntegerField(default=3)
+    blocked = models.BooleanField(default=False)
 
     def __str__(self):
         return self.username
