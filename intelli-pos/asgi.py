1acbc470f01360462ad43201db5614eb3a46aee5
dilonne <muchingamid@gmail.com>
2021-03-19 13:22:10 +0200

    Add .gitignore file Remove compiled pyc files Change project name from camel casing naming

--- intelli-pos/asgi.py
+++ intelli-pos/asgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'intelliPos.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'intelli-pos.settings')
 
 application = get_asgi_application()
