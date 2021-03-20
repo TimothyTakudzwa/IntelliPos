b2d1efdeee8daf646a88188e46402ae665e8981c
Timoth Ngorima <timothytakudzwa@gmail.com>
2021-03-20 11:56:37 +0200

    Remove unnecessary logging

--- merchant/kms_client_api.py
+++ merchant/kms_client_api.py
@@ -63,8 +63,6 @@ def get_jwt_tokens():
 class KMSCLIENTAPI:    
     KMS_BASE_URL = settings.KMS_BASE_URL
     ACCESS_TOKEN, REFRESH_TOKEN = get_jwt_tokens()
-    print(ACCESS_TOKEN)
-    print(REFRESH_TOKEN)
 
     def __init__(self):
         self._headers = None
