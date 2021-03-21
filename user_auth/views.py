6f26facd2416d37aea2a3fa5221c4a4fc847ae1f
dilonne <muchingamid@gmail.com>
2021-03-21 23:55:54 +0200

    Refactor

--- user_auth/views.py
+++ user_auth/views.py
@@ -1,2 +1,18 @@
 
 
+class RegisterView(RegisterView):
+    def get_response_data(self, user):
+        if allauth_settings.EMAIL_VERIFICATION == \
+                allauth_settings.EmailVerificationMethod.MANDATORY:
+            return {"detail": _("Verification e-mail sent."), "token": self.token}
+
+        if getattr(settings, 'REST_USE_JWT', False):
+            data = {
+                'user': user,
+                'token': self.token
+            }
+            logger.info('Successfully Registered User')
+            return JWTSerializer(data).data
+        else:
+            logger.info('Successfully Registered User')
+            return TokenSerializer(user.auth_token).data
