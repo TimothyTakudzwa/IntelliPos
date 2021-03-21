3781f342a0f6e28de5b00058f6a1c308e8e5bdc4
dilonne <muchingamid@gmail.com>
2021-03-21 20:08:35 +0200

    Ovverride Rgister view

--- merchant/views.py
+++ merchant/views.py
@@ -72,6 +72,22 @@ class MerchantProfileViewSet(viewsets.ModelViewSet):
         return [permission() for permission in permission_classes]
 
 
+class RegisterView(RegisterView):
+    def get_response_data(self, user):
+        if allauth_settings.EMAIL_VERIFICATION == \
+                allauth_settings.EmailVerificationMethod.MANDATORY:
+            return {"detail": _("Verification e-mail sent."), "token":self.token}
+
+        if getattr(settings, 'REST_USE_JWT', False):
+            data = {
+                'user': user,
+                'token': self.token
+            }
+            return JWTSerializer(data).data
+        else:
+            return TokenSerializer(user.auth_token).data
+
+
 # Create your views here.
 class TransactionProcessing(APIView):
     """
