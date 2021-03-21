2954ddc9c2d31df9b9c82041c1c1d3013a689957
dilonne <muchingamid@gmail.com>
2021-03-21 21:51:35 +0200

    Refactor

--- user_auth/urls.py
+++ user_auth/urls.py
@@ -2,7 +2,10 @@ from django.urls import path, include
 from allauth.account import views 
 
 
+from .views import *
+
 urlpatterns = [
     path('auth/', include('rest_auth.urls')),
-    path('auth/registration/', include('rest_auth.registration.urls'))
+    # path('auth/registration/', include('rest_auth.registration.urls'))
+    path('auth/registration/', RegisterView.as_view(), name='register'),
 ]
