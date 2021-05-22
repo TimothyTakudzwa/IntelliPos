7709627f4a95f1c753802e1099f8fffe2c17579c
Timoth Ngorima <timothytakudzwa@gmail.com>
2021-05-22 16:38:20 +0200

    Fix Unsupported Datetime and Date Operation

--- user_auth/views.py
+++ user_auth/views.py
@@ -22,7 +22,7 @@ logger = logging.getLogger('gunicorn.error')
 class LoginView(views.LoginView):
 
     def verify_access(self):
-        today = datetime.datetime.now()
+        today = datetime.date.now()
         delta = today - self.user.password_creation_date
         if self.user.locked_out:
             data = {
