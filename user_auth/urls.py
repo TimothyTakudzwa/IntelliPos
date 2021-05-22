48652f494c77c7db5699e6a814a3e59585272a1d
Timoth Ngorima <timothytakudzwa@gmail.com>
2021-05-22 17:22:58 +0200

    Add Password Reset Confirm Path

--- user_auth/urls.py
+++ user_auth/urls.py
@@ -7,7 +7,10 @@ from dj_rest_auth.views import (
 from .views import *
 
 urlpatterns = [
-    path('password/reset/confirm/', PasswordResetConfirmView.as_view(),name='password_reset_confirm'), 
+    path('password/reset/confirm/<uidb64>/<token>', 
+        PasswordResetConfirmView.as_view(),
+        name='password_reset_confirm'
+    ), 
     path('auth/login/', LoginView.as_view(), name='login'),
     path('auth/', include('dj_rest_auth.urls')),
     path('auth/dj-rest-auth/registration/', include('dj_rest_auth.registration.urls')),
