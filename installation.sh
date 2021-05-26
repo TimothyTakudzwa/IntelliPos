d15c87b1ef2da43e663e0af31ee927ace3abc74f
Timoth Ngorima <timothytakudzwa@gmail.com>
2021-05-26 18:35:06 +0200

    Add new line to echo

--- installation.sh
+++ installation.sh
@@ -1,14 +1,14 @@
 #!bin/bash
-echo -n "******************************************************************************************"
+echo -n "******************************************************************************************\n\n"
 echo -n "Updating System Packages"
-echo -n "******************************************************************************************"
+echo -n "\n\n******************************************************************************************"
 sudo yum -y update
 sudo yum -y install yum-utils
 sudo yum -y groupinstall development
 sudo yum install gcc openssl-devel bzip2-devel libffi-devel
 #Get and Instal Python 3.8
-echo -n "******************************************************************************************"
-echo -n "Installing Python"
+echo -n "******************************************************************************************\n\n"
+echo -n "\n\nInstalling Python"
 echo -n "******************************************************************************************"
 sudo yum -y install wget
 cd /opt && sudo wget https://www.python.org/ftp/python/3.8.7/Python-3.8.7.tgz
@@ -18,8 +18,8 @@ sudo ./configure --enable-optimizations
 sudo make altinstall
 sudo rm -f /opt/Python-3.8.7.tgz
 PATH=$PATH:/usr/local/bin
-echo -n "******************************************************************************************"
-echo -n "Installing Virtual Environment"
+echo -n "******************************************************************************************\n\n"
+echo -n "\n\nInstalling Virtual Environment"
 echo -n "******************************************************************************************"
 #Install Virtual Env
 pip3 install virtualenv
@@ -27,7 +27,7 @@ VENV=/home/intellipos/IntelliPos/venv
 virtualenv -p /usr/local/bin/python3.8 $VENV
 source $VENV/bin/activate
 #Install Application Requirements
+echo -n "******************************************************************************************\n\n"
+echo -n "Installing Application Requirements\n\n"
 echo -n "******************************************************************************************"
-echo -n "Installing Application Requirements"
-echo -n "******************************************************************************************"
-pip3 install -r requirements.txt
+cd /home/intellipos/IntelliPos/ && pip3 install -r requirements.txt
