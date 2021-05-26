d5e4ce4bd45431e09ed4e433c36fa9aa155f92c3
Timoth Ngorima <timothytakudzwa@gmail.com>
2021-05-26 18:39:18 +0200

    Print Data

--- installation.sh
+++ installation.sh
@@ -1,15 +1,15 @@
 #!bin/bash
-echo -n "******************************************************************************************\n\n"
-echo -n "Updating System Packages"
-echo -n "\n\n******************************************************************************************"
+echo -e "******************************************************************************************\n\n"
+echo -e "Updating System Packages"
+echo -e "\n\n******************************************************************************************"
 sudo yum -y update
 sudo yum -y install yum-utils
 sudo yum -y groupinstall development
 sudo yum install gcc openssl-devel bzip2-devel libffi-devel
 #Get and Instal Python 3.8
-echo -n "******************************************************************************************\n\n"
-echo -n "\n\nInstalling Python"
-echo -n "******************************************************************************************"
+echo -e "******************************************************************************************\n\n"
+echo -e "\n\nInstalling Python"
+echo -e "******************************************************************************************"
 sudo yum -y install wget
 cd /opt && sudo wget https://www.python.org/ftp/python/3.8.7/Python-3.8.7.tgz
 sudo tar xzf Python-3.8.7.tgz
@@ -18,16 +18,16 @@ sudo ./configure --enable-optimizations
 sudo make altinstall
 sudo rm -f /opt/Python-3.8.7.tgz
 PATH=$PATH:/usr/local/bin
-echo -n "******************************************************************************************\n\n"
-echo -n "\n\nInstalling Virtual Environment"
-echo -n "******************************************************************************************"
+echo -e "******************************************************************************************\n\n"
+echo -e "\n\nInstalling Virtual Environment"
+echo -e "******************************************************************************************"
 #Install Virtual Env
 pip3 install virtualenv
 VENV=/home/intellipos/IntelliPos/venv
 virtualenv -p /usr/local/bin/python3.8 $VENV
 source $VENV/bin/activate
 #Install Application Requirements
-echo -n "******************************************************************************************\n\n"
-echo -n "Installing Application Requirements\n\n"
-echo -n "******************************************************************************************"
+echo -e "******************************************************************************************\n\n"
+echo -e "Installing Application Requirements\n\n"
+echo -e "******************************************************************************************"
 cd /home/intellipos/IntelliPos/ && pip3 install -r requirements.txt
