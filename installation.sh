4a27f6d4b1ef6e24c4300d83296fde4a8ea9c447
Timoth Ngorima <timothytakudzwa@gmail.com>
2021-05-27 11:46:33 +0200

    Add sudo to installation of virtualenv

--- installation.sh
+++ installation.sh
@@ -9,13 +9,13 @@ sudo yum install gcc openssl-devel bzip2-devel libffi-devel
 echo -e "******************************************************************************************\n\n"
 echo -e "Installing Supervisor"
 echo -e "\n\n******************************************************************************************"
-sudo yum install supervisor
+sudo yum -y install supervisor
 sudo systemctl start supervisord
 sudo systemctl enable supervisord
 echo -e "******************************************************************************************\n\n"
 echo -e "Installing Nginx"
 echo -e "\n\n******************************************************************************************"
-sudo yum install nginx
+sudo yum -y install nginx
 sudo systemctl start nginx
 sudo systemctl enable nginx
 #Get and Instal Python 3.8
@@ -34,7 +34,7 @@ echo -e "***********************************************************************
 echo -e "Installing Virtual Environment"
 echo -e "\n\n******************************************************************************************"
 #Install Virtual Env
-pip3 install virtualenv
+sudo pip3 install virtualenv
 VENV=/home/intellipos/IntelliPos/venv
 virtualenv -p /usr/local/bin/python3.8 $VENV --reset-app-data
 source $VENV/bin/activate
@@ -43,3 +43,9 @@ echo -e "***********************************************************************
 echo -e "Installing Application Requirements\n\n"
 echo -e "\n\n******************************************************************************************"
 cd /home/intellipos/IntelliPos/ && pip3 install -r requirements.txt
+sudo mkdir /var/log/intellipos/
+echo -e "******************************************************************************************\n\n"
+echo -e "Creating Log Files \n\n"
+echo -e "\n\n******************************************************************************************"
+touch /var/log/intellipos/access.log
+touch /var/log/intellipos/error.log
