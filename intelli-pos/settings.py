f2f38cadab3f6efa323bd185188f3916e62104fd
Nigel Bongani Zulu <zulunigelb@gmail.com>
2021-08-16 12:46:12 +0200

    add new server to allowed host

--- intelli-pos/settings.py
+++ intelli-pos/settings.py
@@ -19,7 +19,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['18.206.81.87', '127.0.0.1']
+ALLOWED_HOSTS = ['18.206.81.87', '127.0.0.1', '198.211.99.116']
 
 LOGGING = {
     'version': 1,
