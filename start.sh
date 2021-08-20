c321e27d9db16b618ab476fb1d4f84d6c3ecf7c8
Nigel Bongani Zulu <zulunigelb@gmail.com>
2021-08-20 09:39:09 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,1 +1,1 @@
-venv/bin/gunicorn --access-logfile - --workers 3 --bind 0.0.0.0:8007 --log-config gunicorn.conf intelli-pos.wsgi:application 
+venv/bin/gunicorn --access-logfile - --workers 3 --bind 0.0.0.0:8007 intelli-pos.wsgi:application
